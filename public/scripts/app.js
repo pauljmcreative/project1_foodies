14df093f3fb6deb2dc8f954ff426fd9a4edc9771
Paul Mourraille <paul@pauljmphoto.com>
2018-10-09 16:47:30 -0700

    tests zomato api

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,17 +3,41 @@ console.log("app.js is working!");
 const baseUrl = 'http://localhost:3000/api/';
 const users = 'users/';
 const comments = 'comments/';
+const apiKey = '64ec316d35f97e2df01286cf2d5f00df';
 
 
-function removeMe () {
-  console.log("ok i'll remove you");
-  fetch(baseUrl + users + event.target.id)
+ // var zUrl = `https://developers.zomato.com/api/v2.1/search?lat=${pos.lat}&lon=${pos.lng}&cuisines=${cuisines}&sort=real_distance` 
 
+// $.ajax({
+//       url: my_url,
+//       headers: {
+//           "user-key": "64ec316d35f97e2df01286cf2d5f00df"
+//       },
+//       method: 'GET',
+//       dataType: 'json',
+//       success
+
+
+
+$.ajax({
+  method: 'GET',
+  url: 'https://developers.zomato.com/api/v2.1/categories',
+  headers: {
+          "user-key": "64ec316d35f97e2df01286cf2d5f00df"
+      },
+  dataType: 'json',
+  success: successFunction,
+  error: (err) => console.log(err),
+});
+
+function successFunction(response) {
+  console.log(response);
 };
 
 
-$('#delete').on('click', removeMe);
 
+
+//curl -X GET --header "Accept: application/json" --header "user-key: 64ec316d35f97e2df01286cf2d5f00df" "https://developers.zomato.com/api/v2.1/categories"
 
 
 
