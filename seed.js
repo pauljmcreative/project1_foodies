92f8f727fd48be5eb3d6be318b0f7b86a6a1bcae
Paul Mourraille <paul@pauljmphoto.com>
2018-10-09 08:27:40 -0700

    starts seed.js file with seed data

---
+++ seed.js
@@ -0,0 +1,38 @@
+
+
+const db = require('./models');
+
+
+
+let userProfile = [
+{
+  user: 'k-nuggets',
+  password: 'abc123'
+},
+{
+  user: 'poutine4ever',
+  password: 'toronto1'
+},
+{
+  user: 'markymark',
+  password: 'gatorsrule1'
+}
+];
+
+
+
+
+let userComments = [
+{
+  user: 'Kenny B nuggets',
+  message: 'I lie awake at night assured these two will cure cancer based on their development skills.'
+},
+{
+  user: 'Brock Poutine',
+  message: 'I love poutine and I love these guys.  Their work is poutine.'
+},
+{
+  user: 'Mark Floridageorgialine',
+  message: 'Broooooooooooooooooooo!  I love sandwiches.'
+},
+];
