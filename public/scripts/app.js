51833fd45ecc3470c999956894b594ea388d784e
Paul Mourraille <paul@pauljmphoto.com>
2018-10-10 15:08:14 -0700

    makes contact with zomato api and recieves city info

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,46 +1,132 @@
 console.log("app.js is working!");
 
+$(document).ready(function(){
+
+
 const baseUrl = 'http://localhost:3000/api/';
 const users = 'users/';
 const comments = 'comments/';
-const apiKey = '64ec316d35f97e2df01286cf2d5f00df';
-
-
- // var zUrl = `https://developers.zomato.com/api/v2.1/search?lat=${pos.lat}&lon=${pos.lng}&cuisines=${cuisines}&sort=real_distance` 
-
-// $.ajax({
-//       url: my_url,
-//       headers: {
-//           "user-key": "64ec316d35f97e2df01286cf2d5f00df"
-//       },
-//       method: 'GET',
-//       dataType: 'json',
-//       success
+const zomato = 'https://developers.zomato.com/api/v2.1/';
+const zomatoKey = '64ec316d35f97e2df01286cf2d5f00df';
 
 
 
-$.ajax({
-  method: 'GET',
-  url: 'https://developers.zomato.com/api/v2.1/categories',
-  headers: {
-          "user-key": "64ec316d35f97e2df01286cf2d5f00df"
-      },
-  dataType: 'json',
-  success: successFunction,
-  error: (err) => console.log(err),
-});
+///////////SMOOTH SCROLL/////////////////
+///////ask for explanation...
 
-function successFunction(response) {
-  console.log(response);
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      let hash = this.hash;
+      
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 
+
+      800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+
+///////////SMOOTH SCROLL BINDING TO ANCHOR/////////////////
+ $(function() {
+    $('.nav-buttons a').bind('click',function(event){
+      let $anchor = $(this);
+      event.preventDefault();
+      $('.nav-buttons a').removeClass('active');
+      $(this).addClass('active');
+    });
+  });
+
+
+/////SIGNUP FORM//////////////
+
+
+
+
+
+// const getCuisines = () => {
+//     fetch(######)
+//       .then(res => res.json())
+//       .then(cuisines => render(cuisines))
+//       .catch(err => console.log(err));
+//   }
+
+//   getCuisines();
+
+
+// const cuisineSubmit = (event) =>{
+//         event.preventDefault();
+//         console.log('form submitted');
+//         const profileName = $(#username). document.getElementById('username').value;
+//         const profilePassword = document.getElementById('password').value;
+//         const profileFoodPref = document.getElementById('foodPref').value;
+//         const profileData = {username: profileName, password: profilePassword, foodPref: profileFoodPref};
+
+//         fetch(baseUrl, {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json; charset=utf-8",
+//             },
+//             body: JSON.stringify(data),
+//         })
+//             .then(res => res.json())
+//             .then(() => getProjects())
+//             .catch(err => console.log(err));
+//     }
+
+
+// const render = (users) => {
+// console.log(users);
+
+// lat long zomato url:  var zUrl = `https://developers.zomato.com/api/v2.1/search?lat=${pos.lat}&lon=${pos.lng}&cuisines=${cuisines}&sort=real_distance` 
+
+
+//-->find restaurants that match city and country - use zomato api to find rest in city/country
+function findCityId (event) {
+  const query = encodeURI(document.getElementById('cityName').value);
+  let cityId = null;
+  event.preventDefault();
+
+  console.log('Calling Home...')
+  fetch(`${zomato}/cities?q=${query}`, {
+    headers: {
+      "user-key": zomatoKey
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      cityID = data.location_suggestions[0].id;
+      console.log(cityId);
+      getCityRestaurants(cityId)
+    });
+
+//call zomato api
+//get city, country, restaurants
+//if city and country match user input for city and country
+//then show a restaurant that same city and country
+
 };
 
+function getCityRestaurants(cityId) {
+  // ?entity_id=10831&entity_type=city
+  fetch(zomato + 'search', {
+    headers: {
+      "user-key": "64ec316d35f97e2df01286cf2d5f00df"
+    }
+  })
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
+
+$('#cuisine-submit').on('click', findCityId);
 
 
 
-//curl -X GET --header "Accept: application/json" --header "user-key: 64ec316d35f97e2df01286cf2d5f00df" "https://developers.zomato.com/api/v2.1/categories"
 
 
 
-
-
-
+//end of document ready
+});
